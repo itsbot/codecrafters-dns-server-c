6391b9416de963052bd15a3827abdc709367bf15
Patrick Iannini <patrickiannini@knights.ucf.edu>
2024-07-07 11:26:02 -0400

    pass 1st stage

--- app/server.c
+++ app/server.c
@@ -16,7 +16,30 @@ int main() {
     printf("Logs from your program will appear here!\n");
 
     
-	int udpSocket, client_addr_len;struct sockaddr_in clientAddress;udpSocket = socket(AF_INET, SOCK_DGRAM, 0);if (udpSocket == -1) {	printf("Socket creation failed: %s...\n", strerror(errno));	return 1;}// Since the tester restarts your program quite often, setting REUSE_PORT// ensures that we don't run into 'Address already in use' errorsint reuse = 1;if (setsockopt(udpSocket, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {	printf("SO_REUSEPORT failed: %s \n", strerror(errno));	return 1;}struct sockaddr_in serv_addr = { .sin_family = AF_INET ,								 .sin_port = htons(2053),								 .sin_addr = { htonl(INADDR_ANY) },								};if (bind(udpSocket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {	printf("Bind failed: %s \n", strerror(errno));	return 1;
+	int udpSocket, client_addr_len;
+	struct sockaddr_in clientAddress;udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (udpSocket == -1) {	
+		printf("Socket creation failed: %s...\n", strerror(errno));	
+		return 1;
+	}
+	
+	
+	// Since the tester restarts your program quite often, setting REUSE_PORT
+	// ensures that we don't run into 'Address already in use' errors
+	int reuse = 1;
+	if (setsockopt(udpSocket, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {	
+		printf("SO_REUSEPORT failed: %s \n", strerror(errno));	
+		return 1;
+	}
+	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
+									 .sin_port = htons(2053),
+									 .sin_addr = { htonl(INADDR_ANY) },
+									};
+	
+	if (bind(udpSocket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {	
+		printf("Bind failed: %s \n", strerror(errno));	
+		return 1;
+	}
 	int bytesRead;
 	char buffer[512];
 	socklen_t clientAddrLen = sizeof(clientAddress);
